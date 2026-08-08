ab46f2ac4f2094809f195affc9853fb7549cd197
bahihs75 <146964992+bahihs75@users.noreply.github.com>
2026-08-08 19:50:08 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,6 +6,7 @@
 // استيراد المكتبات المطلوبة من Firebase CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // ============================================
 // بيانات التهيئة (تم الحصول عليها من وحدة تحكم Firebase)
@@ -25,3 +26,4 @@ const firebaseConfig = {
 // ============================================
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
